e5ebd78ee444f8273ea26f7c56fe84f6478b731d
savii <savii462@gmail.com>
2024-08-01 15:40:45 -0500

    render

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
-const app = express();
 import spotifyWebApi from 'spotify-web-api-node';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+
+// Get the resolved path to the file and directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
+
 // Serve static files from the "src" directory
 app.use(express.static(path.join(__dirname, 'src')));
 
@@ -16,28 +21,29 @@ app.use(express.static(path.join(__dirname, 'src')));
 app.get('*', (req, res) => { 
   res.sendFile(path.join(__dirname, 'index.html')); 
 });
+
 app.post("/refresh", (req, res) => {
-    const refreshToken = req.body.refreshToken
-    const spotifyApi = new SpotifyWebApi({
-      redirectUri:'https://songtinder.onrender.com',
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new spotifyWebApi({
+      redirectUri: 'https://songtinder.onrender.com',
       clientId: 'CLIENT_ID',
       clientSecret: 'CLIENT_SECRET',
       refreshToken,
-    })
-  
-    spotifyApi
-      .refreshAccessToken()
+    });
+
+    spotifyApi.refreshAccessToken()
       .then(data => {
         res.json({
-          accessToken: data.body.accessToken,
-          expiresIn: data.body.expiresIn,
-        })
+          accessToken: data.body.access_token,
+          expiresIn: data.body.expires_in,
+        });
       })
       .catch(err => {
-        console.log(err)
-        res.sendStatus(400)
-      })
-  })
+        console.error(err);
+        res.sendStatus(400);
+      });
+});
+
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new spotifyWebApi({
