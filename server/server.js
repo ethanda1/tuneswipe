0b94948c41c024359cdc64a629488a9aee301cbc
savii <savii462@gmail.com>
2024-08-01 14:14:59 -0500

    render

--- server/server.js
+++ server/server.js
@@ -50,8 +50,7 @@ app.post('/login', (req, res) => {
         });
 });
 
-const port = process.env.PORT || 3001; 
 
-app.listen(port, () => {
+app.listen(10000, () => {
     console.log(`Server running on port ${port}`);
 });
