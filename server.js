98986597882b20df4c6e14840c83a332dfde3ca4
savii <savii462@gmail.com>
2024-08-01 17:17:37 -0500

    render

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'src')));
 
 // Serve index.html for all other routes
 app.get('*', (req, res) => { 
-  res.sendFile(path.join(__dirname, 'index.html')); 
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 });
 
 app.post("/refresh", (req, res) => {
