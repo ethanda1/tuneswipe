6c09cfcc15e855d6bfb40a395b9fdd73bada3254
savii <savii462@gmail.com>
2024-08-01 15:10:31 -0500

    render

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ import bodyParser from 'body-parser';
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => { 
+  res.sendFile(path.join(__dirname + '/client/build/index.html')) 
+});
 app.post("/refresh", (req, res) => {
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
