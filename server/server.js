60be2ac6a80c6c4c0ac743aff436227d05f21905
savii <savii462@gmail.com>
2024-08-01 15:19:26 -0500

    render

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ import spotifyWebApi from 'spotify-web-api-node';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
-
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 app.use(cors());
 app.use(bodyParser.json());
 // Serve static files from the "src" directory
