354fb79276f4355a0fa2bb0f5ae78e902c62c18c
savii <savii462@gmail.com>
2024-08-03 01:54:33 -0500

    styling edit

--- src/Songcard.jsx
+++ src/Songcard.jsx
@@ -150,12 +150,14 @@ export const Songcard = ({ code }) => {
               className="absolute w-10 right-6 bottom-6 hover:scale-110 transition ease-in-out duration-300 z-20"
               alt="Next"
             />
+            <div className={`${clickedLike ? 'scale-150' : ''}`}>
             <img
               src="/heart-logo-png-transparent.png"
               onClick={handleClickLike}
-              className={`absolute w-10 left-6 bottom-6 hover:scale-110 transition-scale ease-in-out duration-300 z-20 ${clickedLike ? 'scale-150' : ''}`}
+              className={`absolute w-10 left-6 bottom-6 hover:scale-110 transition-scale ease-in-out duration-300 z-20 `}
               alt="Like"
             />
+            </div>
           </div>
         )}
       </div>
